b6c78d9d9c9ffc59a1fe9b05eaedc4e7abc16952
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-05-13 00:33:13 +0100

    Add background image

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import FlatButton from "./Button";
 import FromCountry from "./From/FromCountry";
 import ToCountry from "./To/ToCountry";
 // import { TailwindProvider } from 'tailwind-rn';
 // import utilities from './tailwind.json';
+
+const image = {
+  uri: "https://images.unsplash.com/photo-1612540646801-4d5d183e6bc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+};
 
 export default function App() {
   const [fromCountry, onChangefromCountry] = React.useState("");
@@ -19,20 +23,22 @@ export default function App() {
   return (
     // <TailwindProvider utilities={utilities}>
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <FromCountry
-        FromCountry={fromCountry}
-        onChangeFromCountry={onChangefromCountry}
-        FromCurrency={fromCurrency}
-        onChangeFromCurrency={onChangefromCurrency}
-      />
-      <ToCountry
-        ToCountry={toCountry}
-        onChangeToCountry={onChangetoCountry}
-        ToCurrency={toCurrency}
-        onChangeToCurrency={onChangetoCurrency}
-      />
-      <FlatButton text="Exchange Currency!" onPress={pressHandler} />
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <StatusBar style="auto" />
+        <FromCountry
+          FromCountry={fromCountry}
+          onChangeFromCountry={onChangefromCountry}
+          FromCurrency={fromCurrency}
+          onChangeFromCurrency={onChangefromCurrency}
+        />
+        <ToCountry
+          ToCountry={toCountry}
+          onChangeToCountry={onChangetoCountry}
+          ToCurrency={toCurrency}
+          onChangeToCurrency={onChangetoCurrency}
+        />
+        <FlatButton text="Exchange Currency!" onPress={pressHandler} />
+      </ImageBackground>
     </View>
     // </TailwindProvider>
   );
@@ -42,6 +48,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
     justifyContent: "center",
   },
   greeting: {
