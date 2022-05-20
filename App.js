c2e9c6efd9876537c0d33d4428b4187e3b5c939e
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-05-20 11:15:11 +0100

    Add footer

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import Main from "./Main/Main";
 // import utilities from './tailwind.json';
 
 export default function App() {
-
   const image = {
     uri: "https://images.unsplash.com/photo-1612540646801-4d5d183e6bc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   };
@@ -21,6 +20,7 @@ export default function App() {
         <StatusBar style="auto" />
         <Text style={styles.header}> Currency Exchange</Text>
         <Main />
+        <Text style={styles.footer}> All Rights Reserved By S&S © 2022</Text>
       </ImageBackground>
     </View>
     // </TailwindProvider>
@@ -34,17 +34,28 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-    image: {
-      flex: 1,
-      justifyContent: "center",
-    },
-    header: {
-      width: "90%",
-      alignSelf: "center",
-      textAlign: "center",
-      fontFamily: "Raleway_900Black",
-      fontSize: "3rem",
-      color: "#fffaf0",
-      letterSpacing: 4,
-    },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  header: {
+    width: "90%",
+    alignSelf: "center",
+    textAlign: "center",
+    fontFamily: "Raleway_900Black",
+    fontSize: "3rem",
+    color: "#fffaf0",
+    letterSpacing: 4,
+  },
+  footer: {
+    width: "90%",
+    alignSelf: "center",
+    textAlign: "center",
+    fontFamily: "Raleway_800ExtraBold",
+    fontSize: "1rem",
+    color: "#fffaf0",
+    letterSpacing: 1.5,
+    position: "absolute",
+    bottom: 20,
+  },
 });
