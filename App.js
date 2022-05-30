5a107bb0459fc8a42bd52b3425202ec2eb30866b
Hyosssssun <hyosssssun@gmail.com>
2022-05-30 12:35:57 +0100

    install twrnc

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View, ImageBackground } from "react-native";
 // Components
 import Main from "./Main/Main";
 // Utils
+import tw from 'twrnc';
 // import { TailwindProvider } from 'tailwind-rn';
 // import utilities from './tailwind.json';
 
@@ -14,48 +15,55 @@ export default function App() {
   };
 
   return (
+    // // <TailwindProvider utilities={utilities}>
+    // <View style={styles.container}>
+    //   <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+    //     <StatusBar style="auto" />
+    //     <Text style={styles.header}> Currency Exchange</Text>
+    //     <Main />
+    //     <Text style={styles.footer}> All Rights Reserved By S&S © 2022</Text>
+    //   </ImageBackground>
+    // </View>
+    // // </TailwindProvider>
     // <TailwindProvider utilities={utilities}>
-    <View style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+    <View>
         <StatusBar style="auto" />
-        <Text style={styles.header}> Currency Exchange</Text>
-        <Main />
-        <Text style={styles.footer}> All Rights Reserved By S&S © 2022</Text>
-      </ImageBackground>
+        <Text style={tw`bg-blue-100`}> Currency Exchange</Text>
+        <Text style={tw`bg-red-800`}> All Rights Reserved By S&S © 2022</Text>
     </View>
     // </TailwindProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    width: "100vw",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  header: {
-    width: "90%",
-    alignSelf: "center",
-    textAlign: "center",
-    fontFamily: "Raleway_900Black",
-    fontSize: "3rem",
-    color: "#fffaf0",
-    letterSpacing: 4,
-  },
-  footer: {
-    width: "90%",
-    alignSelf: "center",
-    textAlign: "center",
-    fontFamily: "Raleway_800ExtraBold",
-    fontSize: "1rem",
-    color: "#fffaf0",
-    letterSpacing: 1.5,
-    position: "absolute",
-    bottom: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     width: "100vw",
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   image: {
+//     flex: 1,
+//     justifyContent: "center",
+//   },
+//   header: {
+//     width: "90%",
+//     alignSelf: "center",
+//     textAlign: "center",
+//     fontFamily: "Raleway_900Black",
+//     fontSize: "3rem",
+//     color: "#fffaf0",
+//     letterSpacing: 4,
+//   },
+//   footer: {
+//     width: "90%",
+//     alignSelf: "center",
+//     textAlign: "center",
+//     fontFamily: "Raleway_800ExtraBold",
+//     fontSize: "1rem",
+//     color: "#fffaf0",
+//     letterSpacing: 1.5,
+//     position: "absolute",
+//     bottom: 20,
+//   },
+// });
