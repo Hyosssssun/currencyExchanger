e0df0ee6ff4be689dfb158a00d8966c0bd0e762f
Hyosssssun <hyosssssun@gmail.com>
2022-05-30 16:25:16 +0100

    tailwind style added

--- Main/To/ToContainer.js
+++ Main/To/ToContainer.js
@@ -4,6 +4,8 @@ import { SafeAreaView, StyleSheet } from "react-native";
 // Components
 import CountryInputField from "../../Global/CountryInputField";
 import AmountInputField from "../../Global/AmountInputField";
+// util
+import tw from 'twrnc';
 
 const ToContainer = ({
   toCountry,
@@ -16,7 +18,7 @@ const ToContainer = ({
   onChangetoCurrency,
 }) => {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={tw`justify-center w-full my-4`}>
       <CountryInputField country={toCountry} setCountry={setToCountry} />
       <AmountInputField
         countryFlag={toCountryFlag}
@@ -32,12 +34,12 @@ const ToContainer = ({
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    width: "100%",
-    marginVertical: "1rem",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     justifyContent: "center",
+//     width: "100%",
+//     marginVertical: "1rem",
+//   },
+// });
 
 export default ToContainer;
