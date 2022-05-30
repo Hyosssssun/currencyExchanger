c5060d925ae1f13984d1c60b04b74bfe1627557f
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-05-30 17:16:14 +0100

    Refactor amount input field and button to tailwind css

--- Global/Button.js
+++ Global/Button.js
@@ -1,5 +1,7 @@
+// React
 import React from "react";
-import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+// Expo
 import {
   Raleway_100Thin,
   Raleway_100Thin_Italic,
@@ -20,10 +22,10 @@ import {
   Raleway_900Black,
   Raleway_900Black_Italic,
 } from "@expo-google-fonts/raleway";
-
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
-// import { useTailwind } from "tailwind-rn/dist";
+// Utils
+import tw from "twrnc";
 
 const FlatButton = ({ onPress, text }) => {
   // const tailwind = useTailwind();
@@ -45,40 +47,63 @@ const FlatButton = ({ onPress, text }) => {
     Raleway_700Bold_Italic,
     Raleway_800ExtraBold,
     Raleway_800ExtraBold_Italic,
-    Raleway_900Black
+    Raleway_900Black,
   });
 
   if (!fontsLoaded) {
     return <AppLoading />;
   }
   return (
-    <TouchableOpacity onPress={onPress} style={styles.button}>
-        <Text style={styles.buttonText}>{text}</Text>
+    <TouchableOpacity
+      onPress={onPress}
+      style={tw.style(
+        "w-4/5",
+        "my-4",
+        "py-4",
+        "self-center",
+        "bg-[#252525da]",
+        "rounded-lg",
+        "shadow-neutral-900",
+        "shadow-2xl"
+      )}
+    >
+      <Text
+        style={tw.style(
+          "text-center",
+          "text-[#fffaf0]",
+          "uppercase",
+          "text-lg",
+          "tracking-widest",
+          { fontFamily: "Raleway_800ExtraBold_Italic" }
+        )}
+      >
+        {text}
+      </Text>
     </TouchableOpacity>
   );
 };
 
 export default FlatButton;
 
-const styles = StyleSheet.create({
-  button: {
-    width: "80%",
-    marginVertical: "1rem",
-    paddingVertical: "1rem",
-    alignSelf: "center",
-    backgroundColor: "#252525da",
-    borderRadius: 8,
-    shadowColor: '#151515',
-    shadowOffset: {width: 4, height: 4},
-    shadowOpacity: 0.3,
-    shadowRadius: 8,
-  },
-  buttonText: {
-    textAlign: "center",
-    color: "#fffaf0",
-    textTransform: "uppercase",
-    fontSize: 16,
-    fontFamily: "Raleway_800ExtraBold_Italic",
-    letterSpacing: 2,
-  },
-});
+// const styles = StyleSheet.create({
+//   button: {
+//     width: "80%", // w-4/5
+//     marginVertical: "1rem", // my-4
+//     paddingVertical: "1rem", // py-4
+//     alignSelf: "center", // self-center
+//     backgroundColor: "#252525da",
+//     borderRadius: 8, // rounded-lg
+//     shadowColor: "#151515", // shadow-neutral-900
+//     shadowOffset: { width: 4, height: 4 }, // shadow-2xl
+//     shadowOpacity: 0.3, // shadow-2xl
+//     shadowRadius: 8,
+//   },
+//   buttonText: {
+//     textAlign: "center", // text-center
+//     color: "#fffaf0",
+//     textTransform: "uppercase", // uppercase
+//     fontSize: 16, // text-lg
+//     fontFamily: "Raleway_800ExtraBold_Italic",
+//     letterSpacing: 2, // tracking-wide
+//   },
+// });
